110f6aa4417410fe986a47e5a7d578703cae05ac
DavidMarchant <davidwjmarchant@gmail.com>
2020-04-02 19:23:57 +0100

    Fix option finding logic

--- exec.rb
+++ exec.rb
@@ -2,8 +2,13 @@
 require 'open3'
 
 #treat first argument not starting with a hyphen as the begining of the task
-first_cmd = ARGV.index{ |arg| !arg.start_with?('-') } || 0
-opts_arr, task_arr = ARGV.slice(0, first_cmd), ARGV.slice(first_cmd, ARGV.length)
+first_cmd = ARGV.index{ |arg| !arg.start_with?('-') }
+if first_cmd
+  opts_arr, task_arr = ARGV.slice(0, first_cmd), ARGV.slice(first_cmd, ARGV.length)
+else
+  opts_arr = ARGV
+  task_arr = []
+end
 
 node = ENV['SLURMD_NODENAME']
 
